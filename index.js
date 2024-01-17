42eaaf09b35d199af2aac0fb20d21a96f6353b80
Desmonne Dotson <brazette97@gmail.com>
2024-01-16 20:49:04 -0600

    functions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(activity) {
+    if (activity) {
+        return `This Saturday, I want to ${activity}!`
+    } else {
+        return `This Saturday, I want to roller-skate!`
+    }
+}
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+function wrapAdjective (flair = "*") {
+    return function(adjective = "beautiful") {
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
